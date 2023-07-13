7b449de45fde7ad0f3caecfbb9270499e2addf44
Mariusq <mariusthorstein@gmail.com>
2023-07-13 14:09:59 +0200

    Spring added

---
+++ api-out/src/main/java/com/marius/outEnd/OutEndApplication.java
@@ -0,0 +1,13 @@
+package com.marius.outEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutEndApplication.class, args);
+	}
+
+}
---
+++ api-out/src/test/java/com/marius/outEnd/OutEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marius.outEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
